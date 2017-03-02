a24428d4186b95865d45c57f1d61d5a3090ad53a
Varun Jindal <vjindz@gmail.com>
2017-03-02 18:49:54 -0500

    added fine tuning function

--- v4_Scary_arm/v4_Scary_arm.ino
+++ v4_Scary_arm/v4_Scary_arm.ino
@@ -12,14 +12,11 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // have!
 
 
-uint8_t servonum = 0;
-int potpin = 0;  // analog pin used to connect the potentiometer
-int buttonState = 0;  
 int servo0Pos;
 int servo1Pos;
 int servo2Pos;
-int MOVEDELAY = 20;
-int DISPENSEDELAY = 3;
+int MOVEDELAY = 12;
+char receivedChar;
 
 
 void setup() {
@@ -52,39 +49,30 @@ void moveServos(int pos0, int pos1, int pos2) {
     dir2 = 1;
   } else {
     dir2 = -1;
-  }
-  
-  
+  }  
   while(servo0Pos != pos0) {
     servo0Pos+=dir0;
     pwm.setPWM(0, 0, servo0Pos);
-    delay(10);
+    delay(MOVEDELAY);
   }
   while(servo1Pos != pos1) {
     servo1Pos+=dir1;
     pwm.setPWM(1, 0, servo1Pos);
-    delay(10);
+    delay(MOVEDELAY);
   }
   while(servo2Pos != pos2) {
     servo2Pos+=dir2;
     pwm.setPWM(2, 0, servo2Pos);
-    delay(10);
+    delay(MOVEDELAY);
   }
 }
 
-void bringDown() {
-  for(int i = 250; i < 585; i++) {
-    pwm.setPWM(10, 0, i);
-    pwm.setPWM(11, 0, i);
-    delay(17);
-  }
-}
-
-void rotateDown() {
+// set positions
+void rotatePickUp() {
   pwm.setPWM(4, 0, 150); //rotate (570 up, 150 down)
 }
 
-void rotateUp() {
+void rotateDrop() {
   pwm.setPWM(4, 0, 570); //rotate (570 up, 150 down)
 }
 
@@ -94,44 +82,50 @@ void clawOpen() {
 
 void clawClosed() {
   pwm.setPWM(3, 0, 530); //claw (530 closed, 300 open)
+}
+
+void toDropHeight() {
+  
+}
+
+void toPickUpHeight() {
+  
+}
+
+void toCheckHeight() {
+  // check if pennies right under or not i.e. fine turning height
+  
+}
+
+void fineTune() {
+  while(receivedChar != 'g') {
+    receivedChar = Serial.read();
+    if(receivedChar == 'w') {
+      moveServos(servo0pos + 1, servo1pos, servo2pos);
+    }
+    if(receivedChar == 's') {
+      moveServos(servo0pos - 1, servo1pos, servo2pos);
+    }
+    if(receivedChar == 'r') {
+      moveServos(servo0pos, servo1pos + 1, servo2pos);
+    }
+    if(receivedChar == 'f') {
+      moveServos(servo0pos, servo1pos - 1, servo2pos);
+    }
+    if(receivedChar == 'y') {
+      moveServos(servo0pos, servo1pos, servo2pos + 1);
+    }
+    if(receivedChar == 'h') {
+      moveServos(servo0pos, servo1pos, servo2pos - 1);
+    }
+  }
+}
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bringDown();
-  rotateUp();
-  clawClosed();
-  moveServos(590, 633, false);
-  moveServos(505, 463, true);
-  delay(200);
-  clawOpen();
-  delay(400);
-  rotateDown();
-  delay(1000);
-  moveServos(228, 410, true);
-  delay(2000);
-  clawClosed();
-  delay(200);
-  rotateUp();
-  delay(500);
-  moveServos(246, 473, false);
-  clawOpen();
-  delay(500);
-  moveServos(228, 410, true);
-  rotateDown();
-  delay(200);
-  moveServos(345, 479, true);
-  delay(200);
-  moveServos(344, 502, true);
-  delay(200);
-  clawClosed();
-  moveServos(228, 410, true);
-  rotateUp();
-  moveServos(230, 490, true);
-  clawOpen();
-  delay(500);
-  
-   
-  
+  if(Serial.available() > 0) {
+    
+  }  
 }
